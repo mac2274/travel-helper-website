48ccc874a37cdad01dcce155dbf179a8a4bce1d5
mac2274 <yuen.maggi@gmail.com>
2025-03-13 12:26:50 +0100

    start defining tabWechsel

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,29 @@ overlayContent.addEventListener('click', () => {
     overlay.classList.toggle('hidden');
 })
 
+let tab1 = document.querySelector('#tab1');
+let tab2 = document.querySelector('#tab2');
+let tab3 = document.querySelector('#tab3');
+let tab4 = document.querySelector('#tab4');
+
+/* event-listener hinzufügen */
+tab1.addEventListener('click', () => {
+    tabWechsel(tab1, 1);
+})
+
+tab2.addEventListener('click', () => {
+    tabWechsel(tab2, 2);
+})
+
+tab3.addEventListener('click', () => {
+    tabWechsel(tab3, 3);
+})
+
+tab4.addEventListener('click', () => {
+    tabWechsel(tab4, 4);
+})
+
+/* funktion zum tab-sprung */
+function tabWechsel(tab, zahl){
+    tab.classList.toggle('tab_active');
+}
